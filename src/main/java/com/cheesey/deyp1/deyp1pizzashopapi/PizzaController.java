ec590f30c1f36a154f858e27dab49fa9940a9172
DEYP1 <43216946+DEYP1@users.noreply.github.com>
2018-09-12 14:24:57 -0400

    Update PizzaController.java

--- src/main/java/com/cheesey/deyp1/deyp1pizzashopapi/PizzaController.java
+++ src/main/java/com/cheesey/deyp1/deyp1pizzashopapi/PizzaController.java
@@ -8,6 +8,6 @@ public class PizzaController {
 
     @RequestMapping("/pizzas")
     public String getString(){
-        return "Pizzas are not good.  They are great";
+        return "Pizzas are great.  They are awesome";
     }
 }
