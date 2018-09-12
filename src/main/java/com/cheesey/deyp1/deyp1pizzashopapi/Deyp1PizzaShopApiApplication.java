028c63d48bc0aa1c2c103aac65abff2575ad541d
Dey, Pritam <pritamde@icloud.com>
2018-09-12 13:50:13 -0400

    Initial commit

---
+++ src/main/java/com/cheesey/deyp1/deyp1pizzashopapi/Deyp1PizzaShopApiApplication.java
@@ -0,0 +1,12 @@
+package com.cheesey.deyp1.deyp1pizzashopapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Deyp1PizzaShopApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Deyp1PizzaShopApiApplication.class, args);
+	}
+}
